dfc864d3c7eff344417008d25a0bd968052a4006
Seth Buchanan <seth.buchanan50@gmail.com>
2023-05-29 09:18:52 -0500

    in the middle of working on cversion

--- cversion/mpv-remote.c
+++ cversion/mpv-remote.c
@@ -6,7 +6,10 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+#include <stddef.h>
+
 #include <json-c/json.h>
+#include <mpv/client.h>
 
 struct json_object *parsed_json;
 
@@ -41,72 +44,77 @@ int set_interface_attribs(int fd, int speed) {
     tty.c_cc[VEOL] = 0;
     tty.c_cc[VEOL2] = 0;
     tty.c_cc[VEOF] = 0x04;
-
-    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+ 
+   if (tcsetattr(fd, TCSANOW, &tty) != 0) {
         printf("Error from tcsetattr: %s\n", strerror(errno));
         return -1;
     }
     return 0;
 }
 
-void doMpvCmd(char buf[]) {
+
+void do_mpv_command(char buf[], mpv_handle *handle) {
   struct json_object *name;
-  /* printf("%s\n", buf); */
   json_object_object_get_ex(parsed_json, buf, &name);
   if(name != 0){
-    printf("Called Command: %s\n", json_object_get_string(name));
+    printf("%s", json_object_get_string(name));
+    mpv_command_string(handle ,json_object_get_string(name));
   }
-
+  /* mpv_command_string(handle, "cycle pause"); */
 }
 
-int start_scanning() {
-    char *portname = SERIALTERMINAL;
-    int fd;
 
-    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd < 0) {
-        printf("Error opening %s: %s\n", portname, strerror(errno));
-        return -1;
-    }
-    /*baudrate 9600, 8 bits, no parity, 1 stop bit */
-    set_interface_attribs(fd, B9600);
-
-    /* simple canonical input */
-    while (1) {
-        char buf[83];
-        char *p;
-        int rdlen;
-
-        rdlen = read(fd, buf, sizeof(buf) - 1);
-        if (rdlen > 0) {
-            buf[rdlen] = 0;
-            for (p = buf; rdlen-- > 0; p++) {
-                if (*p < ' ')
-                    *p = '\0';   /* replace any control chars */
-            }
-	    doMpvCmd(buf);
-            /* printf("%s\n", buf); */
-        } else if (rdlen < 0) {
-            printf("Error from read: %d: %s\n", rdlen, strerror(errno));
-        } else {  /* rdlen == 0 */
-            printf("Nothing read. EOF?\n");
-        }               
-    }
-}
-
-int parse_json() {
+void parse_json() {
   FILE *fp;
   char buffer[1024];
 
-  fp = fopen("test.json", "r");
+  fp = fopen("/home/seth/.config/mpv/script-opts/test.json", "r");
   fread(buffer, 1024, 1, fp);
   fclose(fp);
 
   parsed_json = json_tokener_parse(buffer);
-  return 0;
 }
 
-int main() {
-  parse_json();
-  start_scanning();
+
+int mpv_open_cplugin(mpv_handle *handle) {
+  printf("test");
+  /* parse_json(); */
+  char *portname = SERIALTERMINAL;
+  int fd;
+  
+  fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+ 
+  /*baudrate 9600, 8 bits, no parity, 1 stop bit */
+  if (fd < 0) {
+    printf("Error opening %s: %s\n", portname, strerror(errno));
+    return -1;
+  }
+  set_interface_attribs(fd, B9600);
+  
+  /* simple canonical input */
+  while (1) {
+    char buf[83];
+    char *p;
+    int rdlen;
+    mpv_event *event = mpv_wait_event(handle, -1);
+    
+    if (event->event_id == MPV_EVENT_SHUTDOWN){
+      break;
+    }
+    
+    rdlen = read(fd, buf, sizeof(buf) - 1);
+    if (rdlen > 0) {
+      buf[rdlen] = 0;
+      for (p = buf; rdlen-- > 0; p++) {
+	if (*p < ' ') *p = '\0';   /* replace any control chars */
+      }
+      do_mpv_command(buf, handle);
+      /* printf("%s\n", buf); */
+    } else if (rdlen < 0) {
+      printf("Error from read: %d: %s\n", rdlen, strerror(errno));
+    } else {  /* rdlen == 0 */
+      printf("Nothing read. EOF?\n");
+    }
+  }
+  return 0;
 }
