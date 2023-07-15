7e882bce45b97023fc7e220257f4ebd19fb5a82d
Seth Buchanan <seth.buchanan50@gmail.com>
2023-07-14 22:48:17 -0500

    Consitent command reading

--- cversion/mpv-remote.c
+++ cversion/mpv-remote.c
@@ -3,17 +3,18 @@
 #define SERIALTERMINAL      "/dev/ttyACM0"
 #include <errno.h>
 #include <fcntl.h> 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
-#include <stddef.h>
 
 #include <json-c/json.h>
 #include <mpv/client.h>
 
 struct json_object *parsed_json;
+
 
 int set_interface_attribs(int fd, int speed) {
     struct termios tty;
@@ -28,15 +29,15 @@ int set_interface_attribs(int fd, int speed) {
 
     tty.c_cflag |= CLOCAL | CREAD;
     tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;         /* 8-bit characters */
-    tty.c_cflag &= ~PARENB;     /* no parity bit */
-    tty.c_cflag &= ~CSTOPB;     /* only need 1 stop bit */
-    tty.c_cflag &= ~CRTSCTS;    /* no hardware flowcontrol */
+    tty.c_cflag |= CS8;		   /* 8-bit characters */
+    tty.c_cflag &= ~PARENB;        /* no parity bit */
+    tty.c_cflag &= ~CSTOPB;	   /* only need 1 stop bit */
+    tty.c_cflag &= ~CRTSCTS;	   /* no hardware flowcontrol */
 
     tty.c_lflag |= ICANON | ISIG;  /* canonical input */
     tty.c_lflag &= ~(ECHO | ECHOE | ECHONL | IEXTEN);
 
-    tty.c_iflag &= ~IGNCR;  /* preserve carriage return */
+    tty.c_iflag &= ~IGNCR;	   /* preserve carriage return */
     tty.c_iflag &= ~INPCK;
     tty.c_iflag &= ~(INLCR | ICRNL | IUCLC | IMAXBEL);
     tty.c_iflag &= ~(IXON | IXOFF | IXANY);   /* no SW flowcontrol */
@@ -55,11 +56,27 @@ int set_interface_attribs(int fd, int speed) {
 }
 
 
+int handle_mpv_events(mpv_handle *handle) {
+    while (1) {
+      mpv_event *event = mpv_wait_event(handle, 0);
+      printf("event: %s\n", mpv_event_name(event->event_id));
+      
+      if (event->event_id == MPV_EVENT_NONE){
+	break;
+      } else if (event->event_id == MPV_EVENT_SHUTDOWN){
+	return 0;
+      }
+	
+    }
+    return 1;
+}
+
+
 void do_mpv_command(char buf[], mpv_handle *handle) {
   struct json_object *name;
   json_object_object_get_ex(parsed_json, buf, &name);
   if(name != 0){
-    /* printf("%s", json_object_get_string(name)); */
+    printf("%s\n", json_object_get_string(name));
     mpv_command_string(handle ,json_object_get_string(name));
   }
 }
@@ -81,40 +98,42 @@ int mpv_open_cplugin(mpv_handle *handle) {
   parse_json();
   char *portname = SERIALTERMINAL;
   int fd;
-  
-  fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
- 
+  int keep_going = 1;
   /*baudrate 9600, 8 bits, no parity, 1 stop bit */
+  fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+
   if (fd < 0) {
-    printf("Error opening %s: %s\n", portname, strerror(errno));
+    printf("\e[0;36mmpv_remote: Error opening %s, %s\n", portname, strerror(errno));
     return -1;
   }
   set_interface_attribs(fd, B9600);
   
   /* simple canonical input */
-  while (1) {
+  while (keep_going) {
     char buf[83];
     char *p;
     int rdlen;
-    mpv_event *event = mpv_wait_event(handle, -1);
+    /* handle_mpv_events(handle); */
+    mpv_event *event = mpv_wait_event(handle, 0);
+    printf("event: %s\n", mpv_event_name(event->event_id));
     
     rdlen = read(fd, buf, sizeof(buf) - 1);
     if (rdlen > 0) {
       buf[rdlen] = 0;
       for (p = buf; rdlen-- > 0; p++) {
-	if (*p < ' ') *p = '\0';   /* replace any control chars */
+	if (*p < ' ') *p = '\0';   /* replace any control chars with NULL bytes */
       }
       do_mpv_command(buf, handle);
-
     } else if (rdlen < 0) {
       printf("Error from read: %d: %s\n", rdlen, strerror(errno));
     } else {  /* rdlen == 0 */
       printf("Nothing read. EOF?\n");
     }
-    
-    if (event->event_id == MPV_EVENT_SHUTDOWN)
-      break;
 
+    if (event->event_id != MPV_EVENT_NONE){
+      keep_going = handle_mpv_events(handle);
+    }
   }
+  printf("done");
   return 0;
 }
